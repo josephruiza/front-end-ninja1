f8c19a3ac1ed817815c6ffc1aeeaf5ea8358d018
Joseph Ruiz <joseph.ruiz.a@gmail.com>
2017-01-25 23:53:22 +0100

    Se añade la nueva estructura de carpetas

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var sass = require('gulp-sass');
 
 //definimos la tarea por defecto
 gulp.task("default", function(){
-	gulp.watch('./style.scss', ['compile-sass']);	//con watch dejamos alguien vigilando para actualizar los cambios
+	gulp.watch('./src/scss/style.scss', ['compile-sass']);	//con watch dejamos alguien vigilando para actualizar los cambios
 });
 
 //definimos la tarea por defecto
 gulp.task("compile-sass", function(){
-	gulp.src('./style.scss')						//cargo el fichero style.scss
+	gulp.src('./src/scss/style.scss')						//cargo el fichero style.scss
 	.pipe(sass().on('error', sass.logError))		//compilo con sass
-	.pipe(gulp.dest('./'));							//genero el fichero style.css en ./
+	.pipe(gulp.dest('./dist/'));							//genero el fichero style.css en ./
 });
