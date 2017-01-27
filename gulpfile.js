3584af7e71fee7ff5fe09de0582d555c1a9ef45c
Joseph Ruiz <joseph.ruiz.a@gmail.com>
2017-01-27 15:39:32 +0100

    Añado las notificaciones(gulp-notify)

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@
 
 var gulp = require('gulp');	//importamos gulp
 var sass = require('gulp-sass');
+var notify = require('gulp-notify');
 
 //definimos la tarea por defecto
-gulp.task("default", function(){
+gulp.task("default", ["compile-sass"], function(){	//ejecutará previamente las tareas entre []
 	gulp.watch('./src/scss/style.scss', ['compile-sass']);	//con watch dejamos alguien vigilando para actualizar los cambios
 });
 
 //definimos la tarea por defecto
 gulp.task("compile-sass", function(){
-	gulp.src('./src/scss/style.scss')						//cargo el fichero style.scss
+	gulp.src('./src/scss/style.scss')				//cargo el fichero style.scss
 	.pipe(sass().on('error', sass.logError))		//compilo con sass
-	.pipe(gulp.dest('./dist/'));							//genero el fichero style.css en ./
+	.pipe(gulp.dest('./dist/'))						//genero el fichero style.css en ./
+	.pipe(notify("SASS Compilado"));				//nos muestra notificaciones de escritorio
 });
